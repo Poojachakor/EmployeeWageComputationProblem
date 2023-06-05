a3f227fb96ec758ee747ae6d907a7a2bcc09be49
Poojachakor <chakorpooja07@gmail.com>
2023-06-05 18:19:41 +0530

    Add:EmployeeWageComputeProblem

---
+++ EmployeeWageComputeProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageComputeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computaion program");
+        }
+    }
+}
