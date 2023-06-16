3b7a4927e49c601c73496dfd92572f065ddc3c5c
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 16:21:47 +0530

    Add: UC9 file

--- EmployeeWageComputeProblem/Program.cs
+++ EmployeeWageComputeProblem/Program.cs
@@ -6,9 +6,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation.");
-            Employee.ComputeEmployeeWage("Relience",20,20,100);
-            Employee.ComputeEmployeeWage("Nike", 2000, 22, 176);
+            //Employee employee = new Employee();
+            //employee.DailyWage();
+            //Console.WriteLine("Welcome to Employee Wage Computation.");
+            //Employee.ComputeEmployeeWage("Relience", 20, 20, 100);
+            //Employee.ComputeEmployeeWage("Nike", 2000, 22, 176);
 
+
+            EmployeeWage empwage = new EmployeeWage();
+
+            empwage.Employee("Dmart", 20, 15, 80);
+            Console.WriteLine("------------------------------");
+            empwage.Employee("Relience", 25, 10, 60);
         }
+
     }
+    
 }
